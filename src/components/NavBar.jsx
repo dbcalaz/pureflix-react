7066c72cc46aa39482cd76c9665d38d4165ada51
Belén Calaz <dbcalaz@gmail.com>
2025-08-11 15:30:32 -0300

    Hago una petición HTTP de prueba.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,80 @@
-import Imagen  from "./Imagen";
+import Imagen from "./Imagen";
 
-export default function NavBar({pestania, setPestania, setMostrarModalDetalle}){
-    return(
-        <nav onClick={()=> {setMostrarModalDetalle({})}}>
-            <ul>
-                <li className={pestania === "home"?"seleccionado":""} onClick={() => {setPestania("home")}}><Imagen nombreArchivo="home"/><div>Home</div></li>
-                <li className={pestania === "series"?"seleccionado":""} onClick={() => {setPestania("series")}}><Imagen nombreArchivo="peliSerieLan"/><div>Series</div></li>
-                <li className={pestania === "peliculas"?"seleccionado":""} onClick={() => {setPestania("peliculas")}}><Imagen nombreArchivo="peliSerieLan"/><div>Películas</div></li>
-                <li className={pestania === "prox"?"seleccionado":""} onClick={() => {setPestania("prox")}}><Imagen nombreArchivo="peliSerieLan"/><div>Próximos Lanzamientos</div></li>
-                <li className={pestania === "perfil"?"seleccionado":""} onClick={() => {setPestania("perfil")}}><Imagen nombreArchivo="perfil"/><div>Perfil</div></li>
-            </ul>
-        </nav>
-    );
+export default function NavBar({
+  pestania,
+  setPestania,
+  setMostrarModalDetalle,
+}) {
+  function login() {
+    console.log("logueando");
+    fetch("http://127.0.0.1:8080/loginUser?usr=belen&pass=culo")
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+  return (
+    <nav
+      onClick={() => {
+        setMostrarModalDetalle({});
+      }}
+    >
+      <ul>
+        <li
+          className={pestania === "home" ? "seleccionado" : ""}
+          onClick={() => {
+            setPestania("home");
+          }}
+        >
+          <Imagen nombreArchivo="home" />
+          <div>Home</div>
+        </li>
+        <li
+          className={pestania === "series" ? "seleccionado" : ""}
+          onClick={() => {
+            setPestania("series");
+          }}
+        >
+          <Imagen nombreArchivo="peliSerieLan" />
+          <div>Series</div>
+        </li>
+        <li
+          className={pestania === "peliculas" ? "seleccionado" : ""}
+          onClick={() => {
+            setPestania("peliculas");
+          }}
+        >
+          <Imagen nombreArchivo="peliSerieLan" />
+          <div>Películas</div>
+        </li>
+        <li
+          className={pestania === "prox" ? "seleccionado" : ""}
+          onClick={() => {
+            setPestania("prox");
+          }}
+        >
+          <Imagen nombreArchivo="peliSerieLan" />
+          <div>Próximos Lanzamientos</div>
+        </li>
+        <li
+          className={pestania === "perfil" ? "seleccionado" : ""}
+          onClick={() => {
+            setPestania("perfil");
+          }}
+        >
+          <Imagen nombreArchivo="perfil" />
+          <div
+            onClick={() => {
+              login();
+            }}
+          >
+            Perfil
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,4 +1,4 @@
-export default function Pefil(){
+export default function Perfil(){
 
     return(
         <div>
